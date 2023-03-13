615813a0c98a16030df928f7c7c339c86b1ea353
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-03-13 23:59:04 +0100

    blog/index.tsx activeCategory usestate types removed toclear the error on line 131

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -31,9 +31,7 @@ const Blog = () => {
 	const [blogData, setBlogData] = useState<BlogContent[]>(blogDemoData);
 	// const [currentIndex, setCurrentIndex] = useState(0);
 	const sliderRef = useRef<HTMLDivElement>(null);
-	const [activeCategory, setActiveCategory] = useState<
-		"all" | "events" | "academics" | "stories" | "community"
-	>("all");
+	const [activeCategory, setActiveCategory] = useState("all");
 
 	function onNextClick() {
 		if (sliderRef.current) {
@@ -45,7 +43,8 @@ const Blog = () => {
 	function onPrevClick() {
 		if (sliderRef.current) {
 			// let updatedTranslateX = translateX + getTranslateXValue();
-			sliderRef.current.style.transform = `translateX(-100%)`;
+			sliderRef.current.style.transform = `translateX(
+				-100%)`;
 			// setTranslateX(updatedTranslateX);
 		}
 	}
