220b56ef3ddb050a2998acb90cd5268b6d3a3d59
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-03-13 22:39:37 +0100

    faq mobile

--- src/pages/faqs.tsx
+++ src/pages/faqs.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
 import Faq from "../components/Faq";
+import { IoIosArrowDown } from "react-icons/io";
 
 type FaqQuestions = {
 	id: number;
@@ -63,6 +64,18 @@ const Faqs = () => {
 		"general" | "subscriptions" | "prices" | "events" | "sponsorship"
 	>("general");
 
+	const liRef = useRef<HTMLLIElement>(null);
+	const [categoryPlaceholder, setCategoryPlaceholder] = useState("general");
+	const [showCategories, setShowCategories] = useState(false);
+
+	const showCat = () => {
+		// const liValue = liRef.current.textContent;
+		// setCategoryPlaceholder(category);
+		setShowCategories(!showCategories);
+	};
+
+	
+
 	return (
 		<>
 			<Head>
@@ -71,10 +84,10 @@ const Faqs = () => {
 			<div className=' mx-auto max-w-6xl pt-[70px] md:pt-[100px] '>
 				<div className=' mt-6 md:mt-14 '>
 					<div className=' flex flex-col md:flex-row md:justify-between '>
-						<p className='faq_heading text-2xl text-center font-bold text-[#218B39] md:text-5xl '>
+						<p className='faq_heading text-center text-2xl font-bold text-[#218B39] md:text-5xl '>
 							Frequently Asked Questions
 						</p>
-						<div className=' hidden relative md:flex items-center  justify-center '>
+						<div className=' relative hidden items-center justify-center  md:flex '>
 							<input
 								className=' rounded-[3px] border-2 border-[#E1E1E1] px-2 pt-[10px] pb-[7px] outline-none '
 								type='text'
@@ -90,16 +103,22 @@ const Faqs = () => {
 							/>
 						</div>
 					</div>
-					<div className='my-[70px] flex flex-col md:flex-row gap-6'>
-						<div className=' h-fit md:w-1/3 bg-[#D4EDDA] py-10 px-4 md:px-8 '>
-							<p className=' mb-6 text-xl font-bold'>Category</p>
-							<div>
-								{/* <Link href=''></Link> */}
+					<div className='my-[70px] flex flex-col gap-6 px-4 md:flex-row'>
+						<div className='  h-fit bg-[#D4EDDA] px-4 py-2 md:w-1/3 md:py-10 md:px-8 '>
+							<div
+								onClick={showCat}
+								className=' flex cursor-pointer items-center justify-between'>
+								<p className=' font-bold text-[#218B39] md:text-xl'>Category</p>
+								<div>
+									<IoIosArrowDown className=' text-2xl text-[#218B39] ' />
+								</div>
+							</div>
+							<div className={`mt-6  ${ showCategories? "block" : "hidden"} `} >
 								<ul className=' list-none space-y-2'>
 									{CATEGORIES.map((category) => (
-										<li
+										<li ref={liRef}
 											key={category}
-											className={`rounded-[3px] py-[8.5px] pl-8 capitalize ${
+											className={`cursor-pointer rounded-[3px] py-[8.5px] pl-8 capitalize ${
 												category === activeCategory
 													? "bg-[#218B39] text-white"
 													: ""
@@ -111,7 +130,8 @@ const Faqs = () => {
 								</ul>
 							</div>
 						</div>
-						<div className=' md:w-2/3 px-4 md:px-[35px] shadow-lg'>
+						{/* <div className='bg-[#D4EDDA]'>Independence</div> */}
+						<div className=' shadow-lg md:w-2/3 md:px-[35px]'>
 							<div>
 								{faqs
 									.filter((faq) => faq.category === activeCategory)
